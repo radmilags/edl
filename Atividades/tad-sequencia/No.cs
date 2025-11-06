54f092f2f0d2d3e157aeb318a58869a50431f186
radmilags <gama.radmila@gmail.com>
2025-11-06 19:08:41 -0300

    feat: tad-sequencia

---
+++ Atividades/tad-sequencia/No.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class No
+{
+    private object elemento { get; set; }
+    private No? Proximo { get; set; }
+    private No? Anterior { get; set; }
+    
+    public No(object elemento)
+    {
+        this.elemento = elemento;
+        Proximo = null;
+        Anterior = null;
+    }
+}
