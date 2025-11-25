1ace795d90a185075c28d5b3f4156107640e10f0
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2025-11-25 18:16:51 +0000

    fix: deque

--- Atividades/deque-listas/atvd01/Program.cs
+++ Atividades/deque-listas/atvd01/Program.cs
@@ -6,7 +6,30 @@ class Program
     {
         Deque deque = new Deque();
 
-        Console.WriteLine(deque.isEmpty());
+        Console.WriteLine(deque.Size());
+
+        deque.InserirInicio(1);
+        deque.InserirFinal(2);
+        deque.InserirInicio(3);
+        deque.InserirFinal(4);
+
+        Console.WriteLine(deque.Size()); 
+        
+
+        Console.WriteLine(deque.Primeiro()); 
+        Console.WriteLine(deque.Ultimo());    
+
+        Console.WriteLine(deque.RemoverInicio()); 
+        Console.WriteLine(deque.RemoverFim());    
+        
+        Console.WriteLine(deque.Size()); 
+        Console.WriteLine(deque.Primeiro()); 
+        Console.WriteLine(deque.Ultimo());    
+
+        deque.RemoverFim();
+        deque.RemoverInicio();
+
+        Console.WriteLine(deque.Size());
         Console.WriteLine(deque.isEmpty());
     }
 }
