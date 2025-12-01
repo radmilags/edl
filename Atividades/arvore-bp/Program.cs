43e39b298ca7ced23f40c05b8d3d16a672dd5bad
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2025-12-01 16:54:19 -0300

    Update Program.cs

--- Atividades/arvore-bp/Program.cs
+++ Atividades/arvore-bp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Programa
+{
+    public static void Main(string[] args)
+    {
+        ABPArray abp = new ABPArray(30); 
+
+        Console.WriteLine(">>> 1. Inserção Inicial: 10, 5, 15, 2, 8, 22");
+        abp.Insert(10, "Raiz");
+        abp.Insert(5, "Cinco");
+        abp.Insert(15, "Quinze");
+        abp.Insert(2, "Dois");
+        abp.Insert(8, "Oito");
+        abp.Insert(22, "VinteDois");
+        abp.Mostrar();
+
+        Console.Write("Travessia InOrder (Ordenada): ");
+        abp.InOrder(0);
+
+        abp.Insert(25, "VinteCinco");
+        abp.Mostrar();
+        
+        abp.Remove(5);
+        abp.Mostrar();
+
+        Console.Write("Travessia InOrder (Final): ");
+        abp.InOrder(0);
+        Console.WriteLine();
+        Console.WriteLine("Tamanho final: " + abp.size());
+    }
+}
